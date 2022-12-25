e723f7b3a0bbfa22ff7429bc8f4b8943c24fdb30
tsoushi <79128170+tsoushi@users.noreply.github.com>
2022-12-25 18:48:39 +0900

    画像が送信されない問題を解決

--- send.py
+++ send.py
@@ -9,23 +9,29 @@ def send(text, image=None, emergency=False):
     # LineNotifyで送信
     try:
         if config.lineTokens['general']:
+            if image:
+                image.seek(0)
             logger.debug('send to LineNotify(general)')
             lineNotify.send(config.lineTokens['general'], text, image)
         if emergency:
+            if image:
+                image.seek(0)
             if config.lineTokens['emergency']:
                 logger.debug('send to LineNotify(emergency)')
                 lineNotify.send(config.lineTokens['emergency'], text, image)
     except lineNotify.LineNotifyError as e:
         logger.warn(e)
     
-    if image:
-        image.seek(0)
     # Discordで送信
     try:
         if config.discordWebhookUrls['general']:
+            if image:
+                image.seek(0)
             logger.debug('send to discord(general)')
             discordWebhook.send(config.discordWebhookUrls['general'], text, image, imageExt='png')
         if emergency:
+            if image:
+                image.seek(0)
             if config.discordWebhookUrls['emergency']:
                 logger.debug('send to discord(emergency)')
                 discordWebhook.send(config.discordWebhookUrls['emergency'], text, image, imageExt='png')
